bde75460a63af3513621d3f61735764c36e5aee2
jamieotieno <jamie.otieno@student.moringaschool.com>
2026-04-16 23:33:18 +0300

    Add loop through posts array

--- index.js
+++ index.js
@@ -20,7 +20,38 @@ async function fetchPosts() {
   }
 }
 
+// Display Posts
+
+function displayPosts(posts) {
+  const container = document.getElementById("post-list");
+
+  // clear existing content
+  ul.innerHTML = "";  
+
+// Loop through posts array
+   posts.forEach(post => {
+    // create li
+    const li = document.createElement("li");
+
+    // create h1 and set title
+    const h1 = document.createElement("h1");
+    h1.textContent = post.title;
+
+    // create p and set body
+    const p = document.createElement("p");
+    p.textContent = post.body;
+
+    // append h1 and p to li
+    li.appendChild(h1);
+    li.appendChild(p);
+
+    // append li to ul
+    ul.appendChild(li);
+  });
+}
 
 
 // Function Call
-fetchPosts();
+fetchPosts().then(posts => {
+  displayPosts(posts);
+});
