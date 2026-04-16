7290d94e2c5344b2d3bd2848e4b2445f33ff7491
jamieotieno <jamie.otieno@student.moringaschool.com>
2026-04-16 23:16:46 +0300

    Add API Fetch function

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // Write your code here!
+
+// Fetch Data from API
+const apiUrl = "https://jsonplaceholder.typicode.com/"
+
+async function fetchPosts() {
+  try {
+    const response = await fetch(apiUrl);
+     if (!response.ok) {
+      throw new Error(`HTTP error! Status: ${response.status}`);
+    }
+
+    const posts = await response.json();
+
+    console.log("Fetched posts:", posts);
+
+    return posts;
+  } catch (error) {
+    console.error("Error fetching posts:", error);
+  }
+}
+
+
+
+// Function Call
+fetchPosts();
